55e5903b4aa8a60fbf1595f53ae17361679e6cf6
Bruno Alessandro Mieli <86811968+BrunoAMieli@users.noreply.github.com>
2024-06-04 00:51:30 -0300

    Update RESTAPIClientTask.java

--- myinterview/src/main/java/com/example/RESTAPIClientTask.java
+++ myinterview/src/main/java/com/example/RESTAPIClientTask.java
@@ -1,19 +1,35 @@
 package com.example;
 
-/**
- * This task expects you to create an implementation of a Rest API client.
- * Your code should call the API endpoint related below.
- * After receiving the JSON Response, print out how many records exists for each gender
- * API endpoint => https://3ospphrepc.execute-api.us-west-2.amazonaws.com/prod/RDSLambda 
- * 
- * >>> Bonus <<<
- * Generate a CSV file containing many records exists for each gender and save this file to AWS S3 Bucket
- * The filename need to contains your entire name, separated by uderscore. Example: john_lennon.csv
- * AWS S3 bucket name => interview-digiage
- * The credentials you can find in Coodesh platform or ask via e-mail for recrutamento@digiage.com.br
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class RESTAPIClientTask {
 
-    // API endpoint => https://3ospphrepc.execute-api.us-west-2.amazonaws.com/prod/RDSLambda 
+    public static void main(String[] args) {
+        String endpointURL = "https://3ospphrepc.execute-api.us-west-2.amazonaws.com/prod/RDSLambda";
+
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(endpointURL).openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            System.out.println("Resposta da API:");
+            System.out.println(response.toString());
+
+            connection.disconnect();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
